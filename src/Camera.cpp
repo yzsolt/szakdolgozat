3b55e921aa4c692e32a3f9c0f9e603f481b688b5
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-04-06 22:00:11 +0200

    Fixed camera initialization, started implementing right-click drag

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -11,28 +11,67 @@ void Camera::_update() {
 
 	//m_view_matrix = glm::lookAt(m_position, m_position + direction, glm::vec3(0, 1, 0));
 	m_position = -m_direction * m_radius;
-	m_view_matrix = glm::lookAt(m_position, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	m_view_matrix = glm::lookAt(m_position, m_look_at, glm::vec3(0, 1, 0));
 
+}
+
+Camera::Camera() {
+	_update();
 }
 
 void Camera::on_mouse_move(const glm::vec2& movement) {
 
-	if (!m_mouse_control_enabled) {
+	if (!m_left_button_pressed && !m_right_button_pressed) {
 		return;
 	}
 
-	m_yaw_pitch += movement * 0.01f;
+	if (m_left_button_pressed) {
+		m_yaw_pitch += movement * 0.01f;
+	}
+
+	if (m_right_button_pressed) {
+		move(movement);
+	}
 
 	_update();
 
 }
 
-void Camera::set_mouse_control(bool mouse_control) {
-	m_mouse_control_enabled = mouse_control;
+void Camera::set_left_button_pressed(bool left_button_pressed) {
+	m_left_button_pressed = left_button_pressed;
+}
+
+void Camera::set_right_button_pressed(bool right_button_pressed) {
+	m_right_button_pressed = right_button_pressed;
 }
 
 const glm::vec3 Camera::position() const {
 	return m_position;
+}
+
+void Camera::move(const glm::vec2& movement) {
+	/*
+	glm::vec4 right = glm::vec4(glm::normalize(glm::cross(m_direction, glm::vec3(0, 1, 0))), 1);
+
+	float angle = 0.f;
+	if (movement.y != 0.f) {
+		angle = std::tan(movement.x / movement.y);
+
+		if (movement.x < 0) {
+			angle += glm::pi<float>();
+		}
+	}
+
+	glm::mat4 rot = glm::rotate(glm::mat4(1), angle, m_direction);
+
+	right = right * rot;
+	right *= movement.length() / 10.f;
+
+	glm::vec3 movement_3d = glm::vec3(right.x, right.y, right.z);
+
+	m_look_at += movement_3d;
+	m_position += movement_3d;
+	*/
 }
 
 const glm::mat4& Camera::view_matrix() const {
--- src/Camera.h
+++ src/Camera.h
@@ -6,23 +6,32 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 class Camera {
+
 	glm::vec3 m_position;
 	glm::vec3 m_direction;
 	glm::vec2 m_yaw_pitch;
-	//glm::vec3 m_direction;
+	glm::vec3 m_look_at = glm::vec3(0, 0, 0);
 	glm::mat4 m_view_matrix;
-	bool m_mouse_control_enabled = false;
+
+	bool m_left_button_pressed = false;
+	bool m_right_button_pressed = false;
+
 	float m_radius = 10.f;
 
 	void _update();
 
 public:
 
+	Camera();
+
 	void on_mouse_move(const glm::vec2& movement);
 
-	void set_mouse_control(bool mouse_control);
+	void set_left_button_pressed(bool left_button_pressed);
+	void set_right_button_pressed(bool right_button_pressed);
 
 	const glm::vec3 position() const;
+
+	void move(const glm::vec2& movement);
 
 	const glm::mat4& view_matrix() const;
 
