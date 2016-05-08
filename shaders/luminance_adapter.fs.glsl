8d5359a74e1bff317c403d5ec1ac3804a44239c9
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-08 11:15:59 +0200

    Fancy changes

--- shaders/luminance_adapter.fs.glsl
+++ shaders/luminance_adapter.fs.glsl
@@ -7,12 +7,12 @@ uniform sampler2D u_previous_adapted_luminance_texture;
 uniform sampler2D u_average_luminance_texture;
 uniform float u_elapsed_time;
 
-void main()
-{
+void main() {
+
 	float previous_adaptation = texelFetch(u_previous_adapted_luminance_texture, ivec2(0, 0), 0).r;
 	float average_luminance = texelFetch(u_average_luminance_texture, ivec2(0, 0), 6).r;
-	float adaptated_color = previous_adaptation + (average_luminance - previous_adaptation) * (1.0 - pow(0.98, 60.0 * u_elapsed_time));
+	float adaptated_color = previous_adaptation + (average_luminance - previous_adaptation) * (1 - pow(0.98, 60 * u_elapsed_time));
 
-	out_color = vec4(adaptated_color, 0.0, 0.0, 1.0);
+	out_color = vec4(adaptated_color, 0, 0, 1);
 
 }
