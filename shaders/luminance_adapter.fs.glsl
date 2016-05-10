fb1216111a51d786ababea8c28600200beb3c18c
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-10 02:08:07 +0200

    Moved out adaptation speed to a constant

--- shaders/luminance_adapter.fs.glsl
+++ shaders/luminance_adapter.fs.glsl
@@ -7,11 +7,13 @@ uniform sampler2D u_previous_adapted_luminance_texture;
 uniform sampler2D u_average_luminance_texture;
 uniform float u_elapsed_time;
 
+const float ADAPTATION_SPEED = 60.f;
+
 void main() {
 
 	float previous_adaptation = texelFetch(u_previous_adapted_luminance_texture, ivec2(0, 0), 0).r;
 	float average_luminance = texelFetch(u_average_luminance_texture, ivec2(0, 0), 6).r;
-	float adaptated_color = previous_adaptation + (average_luminance - previous_adaptation) * (1 - pow(0.98, 60 * u_elapsed_time));
+	float adaptated_color = previous_adaptation + (average_luminance - previous_adaptation) * (1 - pow(0.98, ADAPTATION_SPEED * u_elapsed_time));
 
 	out_color = vec4(adaptated_color, 0, 0, 1);
 
