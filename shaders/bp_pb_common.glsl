babce016faba371abd927687f0d5a9cf4d33193d
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-15 10:42:57 +0200

    Removed visualization contants from shader

--- shaders/bp_pb_common.glsl
+++ shaders/bp_pb_common.glsl
@@ -20,17 +20,3 @@ struct PhysicallyBasedMaterial {
     TextureMap roughness;
     TextureMap metalness;
 };
-
-vec4 get_texture_map_value(TextureMap texture_map) {
-    return vec4(0);
-}
-
-#define VISUALIZE_NOTHING       0
-#define VISUALIZE_POSITIONS     1
-#define VISUALIZE_NORMALS       2
-#define VISUALIZE_TANGENTS      3
-#define VISUALIZE_AMBIENT       4
-#define VISUALIZE_DIFFUSE       5
-#define VISUALIZE_NORMAL_MAP    6
-#define VISUALIZE_SPECULARITY   7
-#define VISUALIZE_REFLECTIVITY  8
