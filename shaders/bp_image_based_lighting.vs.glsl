7b7956e9ca187636d310ed9d60a32220edd65f84
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-15 11:14:28 +0200

    Use light_common.glsl in BP IBL shader too

--- shaders/bp_image_based_lighting.vs.glsl
+++ shaders/bp_image_based_lighting.vs.glsl
@@ -1,27 +1,11 @@
 
 #version 330 core
 
-layout(location = 0) in vec3 vs_in_position;
-layout(location = 1) in vec3 vs_in_normal;
-layout(location = 2) in vec3 vs_in_tangent;
-layout(location = 3) in vec3 vs_in_bitangent;
-layout(location = 4) in vec2 vs_in_texture;
-
-out gl_PerVertex { vec4 gl_Position; };
+#include "light_common.glsl"
 
 out vec3 vs_out_world_position;
-out vec3 vs_out_world_normal;
-out vec3 vs_out_view_direction;
-out vec2 vs_out_texture;
 
 out mat3 vs_out_tbn;
-
-uniform mat4 u_world;
-uniform mat4 u_view_projection;
-uniform vec3 u_view_position;
-
-uniform mat4 u_world_inverse;
-uniform mat4 u_normal_matrix;
 
 void main() {
 
